d6c76409664f75c89d7785d86210ab1638bb52ba
Ruben Martinez <rubenmartinezTRABALLOS@gmail.com>
2024-10-03 10:14:02 +0200

    refactoring

--- src/Ej04.java
+++ src/Ej04.java
@@ -4,16 +4,25 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Ej04 {
+    static ArrayList<Book> bookList;
+    static Scanner sc;
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        ArrayList<Book> bookList = new ArrayList<>();
+        sc = new Scanner(System.in);
+        bookList = new ArrayList<>();
         System.out.print("Introduzca cuantos libros va a contener el catalogo de la libreria: ");
         int booksNumber = sc.nextInt();
         for (int i = 0; i < booksNumber; i++) {
-            createBook(sc, bookList);
+            createBook();
         }
+        System.out.println("=====================");
+        for (int i = 0; i < bookList.size(); i++) {
+            refillBook(bookList.get(i));
+        }
+        System.out.println("=====================");
+        buyBook();
     }
-    static public void createBook(Scanner sc, ArrayList<Book> bookList) {
+
+    static public void createBook() {
         while (true)    {
             System.out.print("ISBN: ");
             String isbn = sc.next();
@@ -22,9 +31,44 @@ public class Ej04 {
             } else {
                 System.out.println(STR."\{isbn} añadido correctamente.");
                 bookList.add(new Book(isbn));
-                System.out.println(STR."Libros en lista => \{bookList.size()}");
                 break;
             }
         }
+    }
+    static public void refillBook(Book bk) {
+        System.out.print(STR."Numero de ejemplares del libro con ISBN \{bk.getIsbn()}: ");
+        int booksNumber = sc.nextInt();
+        bk.setNumberOfBooks(booksNumber);
+        System.out.println(STR."\{booksNumber} ejemplares anhadidos al libro con ISBN \{bk.getIsbn()}");
+    }
+    static public boolean getBook(String isbn) {
+        return bookList.contains(new Book(isbn));
+    }
+    static public void buyBook() {
+        System.out.println("=====================");
+        System.out.print("ISBN a comprar: ");
+        String searchedISBN = sc.next().trim();
+        if (!getBook(searchedISBN)) {
+            System.out.println("Libro no encontrado :(");
+        }
+        else {
+            Book searchedBook = bookList.get(bookList.indexOf(new Book(searchedISBN)));
+            if (searchedBook.getNumberOfBooks() <= 0)    {
+                System.out.println("Lo sentimos. El libro seleccionado está agotado.");
+            } else {
+                System.out.print(STR."Introduzca la cantidad a adquirir (\{searchedBook.getNumberOfBooks()} ejemplares): ");
+                int buyAmount = sc.nextInt();
+                if (searchedBook.getNumberOfBooks() - buyAmount < 0)    {
+                    System.out.println("No existen suficientes ejemplares para satisfacer la orden de compra");
+                } else {
+                    int oldnumBooks = searchedBook.getNumberOfBooks();
+                    searchedBook.setNumberOfBooks(searchedBook.getNumberOfBooks() - buyAmount);
+                    System.out.println("Felicidades. La orden de compra se ha ejecutado satisfactoriamente.");
+                    System.out.println(STR."Ejemplares del libro \{searchedISBN}: \{oldnumBooks} => \{searchedBook.getNumberOfBooks()}");
+                }
+            }
+        }
+        Book searchedBook = bookList.get(bookList.indexOf(new Book(searchedISBN)) + 1);
+        System.out.println(searchedBook);
     }
 }
--- src/extra_classes/Book.java
+++ src/extra_classes/Book.java
@@ -9,7 +9,24 @@ public class Book {
         this.numberOfBooks = 0;
     }
 
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public int getNumberOfBooks() {
+        return numberOfBooks;
+    }
+
     public void setNumberOfBooks(int numberOfBooks) {
         this.numberOfBooks = numberOfBooks;
+    }
+
+    @Override
+    public String toString() {
+        return STR."Datos del libro: ISBN => \{isbn} || Numero de libros => \{numberOfBooks}";
     }
 }
